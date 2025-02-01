6cee15f02fc579558b8596d2182f106d2b58c872
Samuel581 <00023020@uca.edu.sv>
2025-01-31 23:15:11 -0600

    :sparkles: feat(furniture) implement data table

--- src/app/(dashboard)/furniture/page.tsx
+++ src/app/(dashboard)/furniture/page.tsx
@@ -1,9 +1,11 @@
+import FurnituresDisplayTable from '@/components/features/furniture/FurnituresDisplayTable'
 import React from 'react'
 
 function page() {
   return (
-    <div>
-      Furnitures default page
+    <div className='gap-5'>
+      <h1 className='font-bold text-3xl text-center'>Clientes en sistema</h1>
+      <FurnituresDisplayTable />
     </div>
   )
 }
