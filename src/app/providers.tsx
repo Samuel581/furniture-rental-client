59c809dc51fdc751a5c5144665cae92765ebc9f1
Samuel581 <00023020@uca.edu.sv>
2025-02-20 11:29:25 -0600

    :sparkles: feat(UI) add toaster provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   // We create the client inside useState to ensure it's stable across renders
@@ -11,6 +12,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <Toaster />
     </QueryClientProvider>
   );
 }
