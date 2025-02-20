6739c6c964026b44c32f2b24d78103d047654d33
Samuel581 <00023020@uca.edu.sv>
2025-02-20 11:37:27 -0600

    🚧 feat(combo) add edit combo view

---
+++ src/app/(dashboard)/combos/create/page.tsx
@@ -0,0 +1,12 @@
+import CreateComboForm from '@/components/features/combos/CreateComboForm'
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+      <CreateComboForm />
+    </div>
+  )
+}
+
+export default Page
---
+++ src/components/features/combos/EditComboForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EditComboForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditComboForm
