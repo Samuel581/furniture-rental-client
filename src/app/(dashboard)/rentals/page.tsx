a9f6d4846f5b5b828c269e239126e3e83d47d372
Samuel Adonay Ortiz Carballo <00023020@uca.edu.sv>
2025-04-09 22:03:34 -0600

    ➕ chore(deps): add event-modal

--- src/app/(dashboard)/rentals/page.tsx
+++ src/app/(dashboard)/rentals/page.tsx
@@ -1,10 +1,15 @@
 import RentalsCalendar from '@/components/features/rentals/rentals-calendar'
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { Plus } from 'lucide-react'
 
 function page() {
   return (
     <div>
+      <h1 className='font-bold text-3xl text-center'>Calendario de reservas</h1>
+      <p className='text-center pb-10'>Visualiza las reservas de muebles y eventos futuros</p>
       <RentalsCalendar/>
+      <Button className='mt-5 end-0'><Plus/>Crear renta</Button>
     </div>
   )
 }
--- src/components/features/rentals/rentals-calendar.tsx
+++ src/components/features/rentals/rentals-calendar.tsx
@@ -9,10 +9,12 @@ import {
     createViewMonthGrid,
 } from '@schedule-x/calendar'
 import { createEventsServicePlugin } from '@schedule-x/events-service'
+import { createEventModalPlugin} from '@schedule-x/event-modal'
 import "@schedule-x/theme-default/dist/index.css"
 
 function RentalsCalendar() {
     const eventsService = useMemo(() => createEventsServicePlugin(), [])
+    const eventModal = createEventModalPlugin()
     const calendar = useNextCalendarApp({
         views: [
             createViewMonthGrid(),
@@ -26,20 +28,18 @@ function RentalsCalendar() {
                 id: '1',
                 start: '2025-04-01',
                 end: '2025-04-01',
-                title: 'Event 1',
+                title: 'Pedido de alquiler',
                 description: 'Description for Event 1',
-                location: 'Pedido de alquiler',
             },
             {
                 id: '2',
+                title: 'Pedido para evento privado',
                 start: '2025-04-02',
-                end: '2025-04-04',
-                title: 'Event 2',
+                end: '2025-04-03',
                 description: 'Description for Event 2',
-                location: 'Pedido para evento privado',
             },
         ],
-        plugins: [eventsService],
+        plugins: [eventsService, eventModal],
         callbacks: {
             onRender: () => {
                 eventsService.getAll()
